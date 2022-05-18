2bc3214aaf7cb0d3d55cb09a3d6734e2eef5a3ed
Aniruddha Deb <aniruddha.deb@successive.tech>
2022-05-18 17:41:30 +0530

    first commit

---
+++ projects/mfe1/src/app/todo/todo.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoComponent } from './todo.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    TodoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: TodoComponent,
+      },
+    ]),
+  ],
+    exports: [
+      TodoComponent
+    ]
+})
+export class TodoModule { }
